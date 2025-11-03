98662b30a2e224dc8a1451e4bd7f0c21d334e16b
ujjawalsingh042003-afk <ujjawalsingh042003@gmail.com>
2025-11-03 11:08:45 +0530

    chatbot.py

---
+++ chatbot.py
@@ -0,0 +1,61 @@
+from logging import ERROR
+import random
+import json
+import pickle
+from unittest import result
+import numpy as np
+import nltk
+
+from nltk.stem import WordNetLemmatizer
+from keras.models import load_model
+
+lemmatizer = WordNetLemmatizer
+
+intents = json.loads(open('D:\Chatbot\chatbot\intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
+
+model = load_model('Chatbot_model.h5')
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word, pos='n') for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0]*len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag [i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    ERROR_THESHOLD = 0.25
+    result = [[i, r] for i, r in enumerate(res) if r>ERROR_THESHOLD]
+
+    result.sort(key = lambda x:x[1], reverse = True)
+    return_list = []
+    for r in result:
+        return_list.append({'intent': classes[r[0]],'probability': str(r[1])})
+    return return_list
+
+def get_response(intents_list, intents_json):
+    list_of_intents = intents_json['intents']
+    tag = intents_list[0]['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['response'])
+            break
+    
+    return result
+print("Great! Bot is Running!")
+
+while True:
+    message = input("")
+    ints = predict_class (message)
+    res = get_response (ints, intents)
+    print (res)
